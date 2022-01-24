d09e3af5d5bb2c1c8faa9c4fe3c051c07f1cd368
camilaleite <camilalenas_@hotmail.com>
2022-01-24 20:19:26 -0300

    subindo ex 4

---
+++ script.js
@@ -0,0 +1,21 @@
+//1 - crie uma função que exiba uma mensagem no console
+function camila(){
+    console.log("O teco é o meu melhor amigo")
+}
+camila()
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+function mostrarNome(nome){
+    console.log(`Olá, ${nome}`)
+}
+mostrarNome("Camila")
+//3 - crie uma função que receba um nome, um número, e um estilo musical (parâmetros) e exiba no console
+function dados (nome, numero, estilomusical){
+    console.log(`Meu nome é ${nome}, tenho ${numero} anos e meu estilo musical preferido é ${estilomusical}`)
+}
+dados("Camila","23","Rap/R&B")
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+function infos (filme, musica){
+console.log (`Meu filme preferido é "${filme}" e minha música preferida no momento é ${musica}`)
+}
+infos ("As branquelas","Be alive, da Beyoncé")
